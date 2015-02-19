e3b6d71d44d57eea8857f508ed22b1a631ba2176
Sherrie Jones <sj24kt@comcast.net>
2015-02-19 13:48:05 -0700

    Initial Commit

---
+++ SnapChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SnapChat
+//
+//  Created by Sherrie Jones on 2/19/15.
+//  Copyright (c) 2015 Sherrie Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SnapChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SnapChat
+//
+//  Created by Sherrie Jones on 2/19/15.
+//  Copyright (c) 2015 Sherrie Jones. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
